11a4a8891c09e4a0546d725877adf08f38b06988
iurii <furmaniurii@gmail.com>
2022-12-15 11:44:12 +0400

    Finish app logic

--- src/main.js
+++ src/main.js
@@ -6,10 +6,44 @@ let weather = {
         + "&units=metric&appid=" 
         + this.appKey)
             .then((response) => response.json())
-            .then((data) => console.log(data))
-    }
+            .then((data) => this.displayWeather(data))
+            .catch((err) => this.displayError()) 
+    },
     displayWeather: function(data) {
         const { name } = data;
-        const { icon, description } = data.weather;
-    }
+        const { description, icon } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.city').innerText = `Weather in ${name}`;
+       // document.querySelector('body').style.background-image = `url(https://source.unsplash.com/1600x900/?${name})`;
+        document.querySelector('.icon').src =`https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector('.description').innerText = description;
+        document.querySelector('.temp').innerText = temp + '°C';
+        document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + '%';
+        document.querySelector('.wind').innerText = 'Wind speed: ' + speed + 'km/h';
+        document.querySelector('.weather').classList.remove('loading');
+        document.querySelector('.error').classList.remove('true');
+        document.body.style.backgroundImage= `url(https://source.unsplash.com/1600x900/?${name}`;
+
+    },
+    search: function() {
+       this.fetchWeather(document.querySelector('.search-bar').value);
+    },
+    displayError: function() {
+        document.querySelector('.city').innerText = `Please enter the city name correctly`;
+        document.querySelector('.error').classList.add('true')
+    } 
 }
+
+document.querySelector('.search button').addEventListener('click', function() {
+    weather.search();
+})
+
+document.querySelector('.search-bar').addEventListener('keyup', function(event) {
+   if (event.key == 'Enter') {
+    weather.search();
+   } 
+})
+
+weather.fetchWeather('batumi')
